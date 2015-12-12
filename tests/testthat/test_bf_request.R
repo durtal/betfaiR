c62c7d080d8e7c1bc5d14593cb7c0bd72dd5e315
Tom Heslop <t_heslop@hotmail.co.uk>
2015-12-12 12:34:58 +0000

    add more tests

--- tests/testthat/test_bf_request.R
+++ tests/testthat/test_bf_request.R
@@ -28,7 +28,7 @@ test_that("test that json returned by bf_request contains correct method", {
 
 })
 
-test_that("test that betfaiR helper functions", {
+test_that("test that betfaiR helper functions help", {
 
     req <- betfaiR:::standard_request(method = "listCompetitions")
     expect_is(req, "list")
@@ -37,4 +37,29 @@ test_that("test that betfaiR helper functions", {
     req <- betfaiR:::standard_request(method = "listMarketBook")
     expect_match(req$method, "SportsAPING/v1.0/listMarketBook", ignore.case = TRUE)
 
+})
+
+test_that("test that betfaiR:::build_request correctly formats request", {
+
+    req <- betfaiR:::build_request(req = marketFilter())
+    expect_equal(length(req), 0)
+
+    # test that non-empty request object is a list, and contains names of entered params
+    req1 <- betfaiR:::build_request(req = marketFilter(eventTypeIds = 7,
+                                                       marketCountries = "GB"))
+    expect_equal(length(req1), 2)
+    expect_match(names(req1), "eventTypeIds|marketCountries", ignore.case = TRUE)
+    expect_is(req1$marketCountries, "list")
+
+    # test textQuery field is character, so jsonlite::toJSON correctly formats
+    req2 <- betfaiR:::build_request(req = marketFilter(textQuery = "HELLO WORLD",
+                                                       eventTypeIds = 1))
+    expect_is(req2$eventTypeIds, "list")
+    expect_is(req2$textQuery, "character")
+
+    # test that from|to params are a converted into a list called marketStartTime
+    req3 <- betfaiR:::build_request(req = marketFilter(from = "2015-01-01"))
+    expect_is(req3$marketStartTime, "list")
+    expect_equal(req3$marketStartTime$from, "2015-01-01T00:00:00Z")
+    
 })
