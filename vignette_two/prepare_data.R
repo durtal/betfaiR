136ed4e86aa0dbe9fc8e091fccb7419bd38fa4ea
Tom Heslop <t_heslop@hotmail.co.uk>
2016-02-14 21:41:38 +0000

    update prepare_data.R

--- vignette_two/prepare_data.R
+++ vignette_two/prepare_data.R
@@ -1,5 +1,6 @@
 # load libraries
 library(dplyr)
+library(ggplot2)
 
 # read in collected data, and marketIds (for selectionNames)
 arslei <- readRDS("arslei.RDS")
@@ -14,6 +15,8 @@ arslei <- plyr::ldply(arslei, .fun = function(i) {
     x$collectedAt <- i$collectedAt
     return(x)
 })
+arslei <- arslei %>%
+    left_join(markets$arslei[[1]]$runners)
 
 cittot <- plyr::ldply(cittot, .fun = function(i) {
     x <- plyr::ldply(i$runners, .fun = function(j) {
@@ -22,6 +25,8 @@ cittot <- plyr::ldply(cittot, .fun = function(i) {
     x$collectedAt <- i$collectedAt
     return(x)
 })
+cittot <- cittot %>%
+    left_join(markets$cittot[[1]]$runners)
 
 winner <- plyr::ldply(winner, .fun = function(i) {
     x <- plyr::ldply(i$runners, .fun = function(j) {
@@ -30,3 +35,5 @@ winner <- plyr::ldply(winner, .fun = function(i) {
     x$collectedAt <- i$collectedAt
     return(x)
 })
+winner <- winner %>%
+    left_join(markets$winner[[1]]$runners)
