73ee33ab10e7bbf8e92fbfd522ae3ec311a66d1f
Tom Heslop <t_heslop@hotmail.co.uk>
2016-02-16 23:16:06 +0000

    change a few theme elements

--- vignette_two/theme.R
+++ vignette_two/theme.R
@@ -1,26 +1,26 @@
 theme_betfair <- function(base_size = 10, base_family = "", ...) {
 
     ggplot2::theme(
-        line = ggplot2::element_line(colour = "#000000", size = 0.5, linetype = 1, lineend = "butt"),
+        line =                  ggplot2::element_line(colour = "#000000", size = 0.5, linetype = 1, lineend = "butt"),
         rect =                  ggplot2::element_rect(fill = "#fcfcfc", colour = "#fcfcfc", size = 0.5, linetype = 1),
         text =                  ggplot2::element_text(family = base_family, face = "plain", colour = "#000000",
                                                       size = base_size, hjust = 0.5, vjust = 0.5, angle = 0,
                                                       lineheight = 0.9),
-        axis.text =             ggplot2::element_text(size = ggplot2::rel(0.9), colour = "#000000"),
-        strip.text =            ggplot2::element_text(size = ggplot2::rel(0.9)),
+        axis.text =             ggplot2::element_text(size = ggplot2::rel(1), colour = "#000000"),
+        strip.text =            ggplot2::element_text(size = ggplot2::rel(1)),
         axis.line =             ggplot2::element_blank(),
-        axis.text.x =           ggplot2::element_text(vjust = 1, size = ggplot2::rel(0.8)),
-        axis.text.y =           ggplot2::element_text(hjust = 1, size = ggplot2::rel(0.8)),
+        axis.text.x =           ggplot2::element_text(vjust = 1, size = ggplot2::rel(0.9)),
+        axis.text.y =           ggplot2::element_text(hjust = 1, size = ggplot2::rel(0.9)),
         axis.ticks =            ggplot2::element_blank(),
-        axis.title.x =          ggplot2::element_text(size = ggplot2::rel(0.9)),
-        axis.title.y =          ggplot2::element_text(size = ggplot2::rel(0.9), angle = 90),
+        axis.title.x =          ggplot2::element_text(size = ggplot2::rel(0.95)),
+        axis.title.y =          ggplot2::element_text(size = ggplot2::rel(0.95), angle = 90),
         legend.background =     ggplot2::element_rect(color = "#fcfcfc"),
         legend.margin =         grid::unit(0.2, "cm"),
         legend.key =            ggplot2::element_rect(fill = "#fcfcfc", colour = "#d8d8d8"),
         legend.key.size =       grid::unit(1.2, "lines"),
         legend.key.height =     NULL,
         legend.key.width =      NULL,
-        legend.text =           ggplot2::element_text(size = ggplot2::rel(0.9)),
+        legend.text =           ggplot2::element_text(size = ggplot2::rel(0.95)),
         legend.text.align =     NULL,
         legend.title =          ggplot2::element_blank(),
         legend.position =       "bottom",
@@ -37,7 +37,7 @@ theme_betfair <- function(base_size = 10, base_family = "", ...) {
         strip.text.x =          ggplot2::element_text(),
         strip.text.y =          ggplot2::element_text(angle = -90),
         plot.background =       ggplot2::element_rect(fill = "#fcfcfc"),
-        plot.title =            ggplot2::element_text(size = ggplot2::rel(1.1), colour = "#000000"),
+        plot.title =            ggplot2::element_text(size = ggplot2::rel(1.2), colour = "#000000", hjust = 0),
         plot.margin =           grid::unit(c(1, 1, 0.5, 0.5), "lines")
     )
 }
