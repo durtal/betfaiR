0d7f0cfaa0ffc7da739e45fe699e2c3899e38430
Tom Heslop <t_heslop@hotmail.co.uk>
2016-03-04 21:31:02 +0000

    a little progress

--- R/collect_data.R
+++ R/collect_data.R
@@ -1,7 +1,25 @@
-# collect_data <- function(bf, marketIds, files) {
+# collect_data <- function(bf,
+#                          marketIds,
+#                          files,
+#                          maxResults = 10,
+#                          priceProjection = c("EX_ALL_OFFERS", "EX_TRADED")) {
 #
-#     plyr::l_ply(available_markets, .fun = function(i, bf, files) {
+#     available_markets <- bf$marketCatalogue(filter = marketFilter(marketIds = marketIds),
+#                                             maxResults = maxResults)
+#     available_markets <- sapply(available_markets, function(i) i$market$marketId)
+#     test <- marketIds %in% available_markets
+#     marketIds <- marketIds[test]
 #
+#     plyr::l_ply(marketIds, .fun = function(i, bf, files) {
+#
+#         filename <- paste0("market_", i, ".RDS")
+#         if(filename %in% files) {
+#             previous_data <- readRDS(filename)
+#         } else {
+#             previous_data <- list()
+#         }
+#         current_data <- bf$marketBook(marketIds = i,
+#                                       priceProjection = priceProjection)
 #
 #     }, bf = bf, files = files)
 #
