b6d3e5374cb8c409a1e2de85b8be7ced4957bed2
Tom Heslop <t_heslop@hotmail.co.uk>
2016-03-22 21:17:17 +0000

    add a little more

--- R/collect_data.R
+++ R/collect_data.R
@@ -13,18 +13,29 @@
 # #' runners in the markets you are collecting data from.
 # #'
 # #' @param bf betfair environment, returned from the \link{betfair} function
+# #' @param filter \link{marketFilter} object to help retrieve marketIds for markets
+# #' whose data is to be collected
 # #' @param marketIds ids of markets to retrieve data for
-# #' @param marketIds_file filename for markets collecting data from
+# #' @param maxResults maximum number of markets to retrieve
+# #' @param marketIds_file filename for markets collecting data from, default is
+# #' "marketIds" which will create a custom file which includes the date on which
+# #' the file was created, like so "yyyy-mm-dd_marketIds.RDS"
 # #' @param priceProjection what data to return via the \link{marketBook} function
+# #' @param marketProjection what data is to be returned via the \link{marketCatalogue}
+# #' function, which is used to retrieve metadata about the markets and establish
+# #' the live markets
 # #'
 # #' @return the function saves data returned from the Betfair API into RDS files
 # #' named after the market, for example market_1.121212121.RDS.
 # #'
 # #' @export
 # collect_data <- function(bf,
-#                          marketIds,
+#                          filter = marketFilter(),
+#                          marketIds = NULL,
+#                          maxResults = 1000,
 #                          marketIds_file = "marketIds",
-#                          priceProjection = c("EX_ALL_OFFERS", "EX_TRADED")) {
+#                          priceProjection = c("EX_ALL_OFFERS", "EX_TRADED"),
+#                          marketProjection = c("EVENT", "RUNNER_DESCRIPTION")) {
 #
 #     # retrieve files from current working directory
 #     files <- list.files()
@@ -32,10 +43,19 @@
 #     # create marketIds file name using marketIds_file param
 #     marketIds_file <- paste0(today, "_", marketIds_file, ".RDS")
 #     if(!(marketIds_file %in% files)) {
-#         out <- bf$marketCatalogue(filter = marketFilter(marketIds = marketIds),
-#                                   maxResults = length(marketIds),
+#         # build filter object
+#         filter_obj <- filter
+#         # overwrite filter object if marketIds are present in params
+#         if(!is.null(marketIds)) filter_obj <- marketFilter(marketIds = marketIds)
+#         # retrieve data
+#         out <- bf$marketCatalogue(filter = filter_obj,
+#                                   maxResults = maxResults,
 #                                   marketProjection = c("EVENT", "RUNNER_DESCRIPTION"))
 #         saveRDS(out, marketIds_file)
+#         marketIds <- sapply(out, function(i) i$market$marketId)
+#     } else {
+#         marketIds <- readRDS(marketIds_file)
+#         marketIds <- sapply(marketIds, function(i) i$market$marketIds)
 #     }
 #     # ascertain which markets are still live
 #     available_markets <- bf$marketCatalogue(filter = marketFilter(marketIds = marketIds),
