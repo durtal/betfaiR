24391ae8c1a21e6ff68fe1b67729b5b0d692fb0a
Tom Heslop <t_heslop@hotmail.co.uk>
2016-03-23 19:43:18 +0000

    add betfair_theme and improve plot of statement

--- R/print_summary.R
+++ R/print_summary.R
@@ -313,12 +313,58 @@ plot.account_statement <- function(x, ...) {
     })
     df <- plyr::ldply(df)
     df$itemDate <- strptime(df$itemDate, "%Y-%m-%dT%H:%M:%S")
-    ggplot2::ggplot(data = df) +
+    startDate <- strptime(min(df$itemDate), "%Y-%m-%d")
+    endDate <- strptime(max(df$itemDate), "%Y-%m-%d")
+    title <- "Betfair Balance - Profit/Loss"
+    subtitle <- paste0(startDate, " to ", endDate)
+    p <- ggplot2::ggplot(data = df) +
         ggplot2::geom_path(ggplot2::aes(x = df$itemDate,
                                         y = df$balance),
                            col = "#D9220F",
-                           size = 1.1) +
+                           size = 1.1)
+    p <- p +
         ggplot2::labs(x = "Date",
-                      y = "Balance") +
-        ggplot2::theme_light()
+                      y = "Balance",
+                      title = title)
+    p <- p +
+        betfair_theme()
+    p <- p +
+        ggplot2::ggtitle(bquote(atop(bold(.(title)), atop(.(subtitle)))))
+    p
+}
+
+#' betfair_theme
+#'
+#' @description ggplot2 theme for betfaiR package plots, theme is used to plot the
+#' output of the \link{statement} method
+#'
+#' @param base_size font size
+#' @param base_family font family
+#'
+#' @export
+betfair_theme <- function(base_size = 10, base_family = "") {
+    ggplot2::theme(
+        line = ggplot2::element_line(colour = "#E1E1D0",
+                                     linetype = 1,
+                                     lineend = "butt"),
+        rect = ggplot2::element_rect(fill = "#FFFFFF",
+                                     colour = "#E1E1D0",
+                                     size = 1,
+                                     linetype = 1),
+        text = ggplot2::element_text(family = base_family,
+                                     face = "plain",
+                                     size = base_size,
+                                     colour = "#000000"),
+        panel.background = ggplot2::element_rect(fill = "#FFFFFF"),
+        plot.background = ggplot2::element_rect(fill = "#FFFFFF"),
+        axis.text = ggplot2::element_text(size = ggplot2::rel(0.80)),
+        axis.title = ggplot2::element_text(size = ggplot2::rel(0.90)),
+        plot.title = ggplot2::element_text(size = ggplot2::rel(1.05),
+                                           face = "bold",
+                                           hjust = 0),
+        panel.grid.major = ggplot2::element_line(colour = "#E1E1D0"),
+        axis.ticks = ggplot2::element_line(colour = "#E1E1D0"),
+        axis.title = ggplot2::element_text(colour = "#000000",
+                                           face = "bold")
+    )
 }
